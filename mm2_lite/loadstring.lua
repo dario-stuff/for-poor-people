cf7f9929292b88e45b31b39d38b0c4f4ecfd14ef
dario-stuff <hitlerismyfriend123@gmail.com>
2024-10-12 12:11:46 +0200

    Create loadstring.lua

---
+++ mm2_lite/loadstring.lua
@@ -0,0 +1,11 @@
+--[[
+
+ __  __ __  __ ____    _     _ _       
+|  \/  |  \/  |___ \  | |   (_) |_ ___ 
+| |\/| | |\/| | __) | | |   | | __/ _ \
+| |  | | |  | |/ __/  | |___| | ||  __/
+|_|  |_|_|  |_|_____| |_____|_|\__\___|
+
+--]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dario-stuff/for-poor-people/refs/heads/main/mm2_lite/loader-free.lua", true))()
